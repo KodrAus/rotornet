4c939dbae37d0a623c64719934c28d0d8d2d0cf1
Ashley Mannix <ashleymannix@live.com.au>
2016-07-05 16:04:22 +1000

    mocking loop api

--- samples/Rotor.Fsm.Basic/Program.fs
+++ samples/Rotor.Fsm.Basic/Program.fs
@@ -1,8 +1,28 @@
-﻿//TODO: Sample building a loop with an FSM in it (same as idle basic sample): run until state is 0
+﻿//TODO: Write a machine that ticks 3 times, then ends
 
-open System
+open Rotor.Fsm
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World!"
+    //Pretty much the API we want to be using
+    //Will need to figure out the self returns for object expressions
+    //Worst case we just won't be able to use them
+    let l = seq {
+        new IMachine with
+            member m.Create c s =
+                printfn "Created"
+                Some(Ok(m))
+            member m.Ready c s =
+                printfn "Ready"
+                Some(Ok(m))
+            member m.Wakeup c s =
+                printfn "Wakeup"
+                Some(Ok(m))
+            member m.Timeout c s =
+                printfn "Timeout"
+                Some(Ok(m))
+    } 
+    |> loop 
+    |> run
+
     0
