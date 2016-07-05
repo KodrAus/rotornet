e31c49f0bac1597b67d0d9b5d2efc0326fe33336
Ashley Mannix <ashleymannix@live.com.au>
2016-07-05 16:53:59 +1000

    mocking initial api

--- samples/Rotor.Fsm.Basic/Program.fs
+++ samples/Rotor.Fsm.Basic/Program.fs
@@ -1,26 +1,42 @@
 ﻿//TODO: Write a machine that ticks 3 times, then ends
+//TODO: Work out how to return the intent
 
 open Rotor.Fsm
+
+type Counter<'c, 's> (state) =
+    member this.State = state
+
+    interface IMachine<'c, 's> with
+        //When a machine is created internally for a socket
+        //This is not something you call yourself
+        //In this case, we construct the machine ourselves so don't need this
+        member this.Create c s =
+            done
+
+        //When a machine is ready to run
+        //This corresponds to a socket ready call
+        member this.Ready m c s =
+            printfn "Ready"
+            deadline Counter(3) 1000
+
+        //When a machine receives a wakeup
+        member this.Wakeup m c s =
+            done
+
+        //When a machine timeout occurs
+        member this.Timeout m c s =
+            match m.State with
+            | x when x < 0 -> error "state was less than 0" //We expect the counter to be greater than 0
+            | 0 -> done //If the counter is less than 0, we close this machine
+            | x -> deadline Counter(x - 1) 1000 //If the counter is greater than 0, we continue
 
 [<EntryPoint>]
 let main argv = 
     //Pretty much the API we want to be using
     //Will need to figure out the self returns for object expressions
     //Worst case we just won't be able to use them
-    let l = seq {
-        new IMachine with
-            member m.Create c s =
-                printfn "Created"
-                Some(Ok(m))
-            member m.Ready c s =
-                printfn "Ready"
-                Some(Ok(m))
-            member m.Wakeup c s =
-                printfn "Wakeup"
-                Some(Ok(m))
-            member m.Timeout c s =
-                printfn "Timeout"
-                Some(Ok(m))
+    seq {
+        Counter(3)
     } 
     |> loop 
     |> run
--- src/Rotor.Fsm/Program.fs
+++ src/Rotor.Fsm/Program.fs
@@ -2,7 +2,19 @@
 ///
 /// Base state machine implementation for asynchronous io
 
+//State: First Principals implementation
+
 //TODO: Use KestrelThread as a base for building up this layer
+
+//Need a loop construct that takes a sequence of machines, registers libuv handles for them and executes certain events
+//Need an IMachine construct that defines the base operations available on a machine, and how they're linked to libuv events
+//Need to close the loop when there are no active machines
+//Need a Response enum with the fo
+
+//LibuvMachine(Libuv stuff, Option<Machine>)
+//When Option<Machine> is None, remove it
+//When not Machines.Any m.Machine.isSome, close the loop
+//Machines store their own state info, each op returns a new machine
 
 namespace Rotor
 
@@ -10,4 +22,16 @@ module Fsm =
     open System
     open Rotor.Libuv
 
-    let fsm = printfn "Hello"
+    type Response<'m> =
+    | Ok of 'm
+    | Done
+    | Error of string
+    | Deadline of 'm * TimeSpan
+
+    //NOTE: This is going to require an ugly type cast, maybe we should pass a state arg?
+    //This isn't really using F# effectively, we have the accessible state, but aren't using it on types. Needs help
+    type IMachine<'c, 's> =
+        abstract member Create :    IMachine<'c, 's> * 'c * 's -> Response<IMachine<'c, 's>>
+        abstract member Ready :     IMachine<'c, 's> * 'c * 's -> Response<IMachine<'c, 's>>
+        abstract member Wakeup :    IMachine<'c, 's> * 'c * 's -> Response<IMachine<'c, 's>>
+        abstract member Timeout :   IMachine<'c, 's> * 'c * 's -> Response<IMachine<'c, 's>>
